0e6f6cfd40bb3015887f32d1ce02ecccf0dfdff0
Soham Mozar <soham@Sohams-iMac.local>
2017-06-17 05:33:02 +0530

    Compeleted setting up firebase

---
+++ Sociall/SigninVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sociall
+//
+//  Created by Soham Mozar on 16/06/17.
+//  Copyright © 2017 abc. All rights reserved.
+//
+
+import UIKit
+
+class SigninVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
